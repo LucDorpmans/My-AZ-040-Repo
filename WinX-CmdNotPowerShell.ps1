080c90a59defb10dd0e36f1ee3139f89fc1e29f2
LucDorpmans <GitHub@sure.nl>
2022-10-25 00:02:04 +0200

    Upd

---
+++ WinX-CmdNotPowerShell.ps1
@@ -0,0 +1,4 @@
+﻿# HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\DontUsePowerShellOnWinX
+Get-ItemProperty -path  HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+Set-ItemProperty -path  HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -Value 1
+Get-Process explorer | Stop-Process

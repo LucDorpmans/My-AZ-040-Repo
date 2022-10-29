505f3838571825b3463303116d0a3cb8d63de6af
LucDorpmans <GitHub@sure.nl>
2022-10-29 19:26:42 +0200

    U

--- GetMy-AZ-040-Scripts.ps1
+++ GetMy-AZ-040-Scripts.ps1
@@ -1,7 +1,7 @@
 # GetMy-AZ-040-Scripts.ps1
 Function Get-MyScript { Param( [string]$AScript,[switch]$EditFile = $False, 
 							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-AZ-040-Repo/main/$AScript"  -Outfile "$SPath$AScript" 
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-AZ-040-Repo/main/ConfigScripts$AScript"  -Outfile "$SPath$AScript" 
 If ($EditFile) { PSEdit ("$SPath$AScript" )} }
 
 Get-MyScript "PowerShell-Core-7-Download+Install.ps1" -EditFile
@@ -11,6 +11,3 @@ Get-MyScript "AddRSATToolsList-Win1x.ps1" -EditFile
 Get-MyScript "GitHub-Desktop-Download+SilentInstall.ps1" -EditFile
 
 # WinGet install Microsoft.VisualStudioCode
-# Get the Install-VSCode script"
-# Invoke-Webrequest -Uri "https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1" -Outfile "$env:USERPROFILE\Downloads\Install-VSCode.ps1"
-# PSEdit  ("$env:USERPROFILE\Downloads\Install-VSCode.ps1")

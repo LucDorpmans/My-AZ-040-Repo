12c6db75162c8dd306f24ac89f41f6c06a530a11
LucDorpmans <GitHub@sure.nl>
2022-10-30 14:52:35 +0100

    Upd

--- GetMy-AZ-040-Scripts.ps1
+++ GetMy-AZ-040-Scripts.ps1
@@ -1,7 +1,7 @@
 # GetMy-AZ-040-Scripts.ps1
 Function Get-MyScript { Param( [string]$AScript,[switch]$EditFile = $False, 
 							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-AZ-040-Repo/main/ConfigScripts$AScript"  -Outfile "$SPath$AScript" 
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-AZ-040-Repo/main/ConfigScripts/$AScript"  -Outfile "$SPath$AScript" 
 If ($EditFile) { PSEdit ("$SPath$AScript" )} }
 
 Get-MyScript "PowerShell-Core-7-Download+Install.ps1" -EditFile

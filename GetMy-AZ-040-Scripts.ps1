450388b042e3041ab46bf62d529e3fc260737af9
LucDorpmans <GitHub@sure.nl>
2022-10-30 18:42:53 +0100

    Upd

--- GetMy-AZ-040-Scripts.ps1
+++ GetMy-AZ-040-Scripts.ps1
@@ -4,11 +4,14 @@ Function Get-MyScript { Param( [string]$AScript,[switch]$EditFile = $False,
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-AZ-040-Repo/main/ConfigScripts/$AScript"  -Outfile "$SPath$AScript" 
 If ($EditFile) { PSEdit ("$SPath$AScript" )} }
 
-Get-MyScript "PowerShell-Core-7-Download+Install.ps1" 
-Get-MyScript "Edge-HideFirstRunExperience.ps1" -EditFile
-Get-MyScript "WinX-CmdNotPowerShell.ps1" -EditFile
-Get-MyScript "RSAT-W1x-AZ-040.ps1" -EditFile
-Get-MyScript "GitHub-Desktop-Download+SilentInstall.ps1" -EditFile
+Invoke-WebRequest -uri "https://github.com/LucDorpmans/$MyRepo/archive/refs/heads/main.zip" -OutFile "$env:USERPROFILE\Downloads\$MyRepo.zip"
+Expand-Archive "$env:USERPROFILE\Downloads\$MyRepo.zip" -DestinationPath E:\ -Force
+Rename 
+# Get-MyScript "PowerShell-Core-7-Download+Install.ps1" 
+# Get-MyScript "Edge-HideFirstRunExperience.ps1" -EditFile
+# Get-MyScript "WinX-CmdNotPowerShell.ps1" -EditFile
+# Get-MyScript "RSAT-W1x-AZ-040.ps1" -EditFile
+# Get-MyScript "GitHub-Desktop-Download+SilentInstall.ps1" -EditFile
 
 # WinGet install Microsoft.VisualStudioCode
 # WinGet install Microsoft.WindowsTerminal

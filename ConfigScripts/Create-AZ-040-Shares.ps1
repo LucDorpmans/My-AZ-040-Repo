f07c1b7673a1a71b03225419b14dbb646ca61271
LucDorpmans <GitHub@sure.nl>
2022-10-30 17:01:18 +0100

    Add LabFiles.org

---
+++ ConfigScripts/Create-AZ-040-Shares.ps1
@@ -0,0 +1,6 @@
+﻿Get-SmbShare MyAllFiles | Remove-SmbShare -Confirm:$false
+Get-SmbShare MyLabFiles | Remove-SmbShare -Confirm:$false
+
+New-SmbShare -Name MyFiles -Path E:\MyFiles
+New-SmbShare -Name MyAllFiles -Path E:\MyFiles\My-AZ-040-AllFiles
+New-SmbShare -Name MyLabFiles -Path E:\MyFiles\My-AZ-040-LabFiles

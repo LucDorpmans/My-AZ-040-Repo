d3005dbc47a72c339e7b8f6372ced1dec88db6ae
LucDorpmans <GitHub@sure.nl>
2022-11-02 22:15:51 +0100

    U

--- ConfigScripts/Connect-AZ-040-SMBMappings.ps1
+++ ConfigScripts/Connect-AZ-040-SMBMappings.ps1
@@ -2,7 +2,7 @@
 
 $MyRepo = "My-AZ-040-Repo-main"
 $VolE = Get-Volume -DriveLetter "E" -EA SilentlyContinue
-If (!($Vole.DriveType -eq "Fixed")) { New-SmbMapping -LocalPath E: -RemotePath \\LON-DC1\MyRepo\LabFiles.org\LON-CL1 }
+If (!($Vole.DriveType -eq "Fixed")) { New-SmbMapping -LocalPath E: -RemotePath \\LON-DC1\MyRepo\LabFiles.org\LabFiles.CL1 }
 New-SmbMapping -LocalPath F: -RemotePath \\LON-DC1\MyRepo\My-AZ-040-AllFiles
 New-SmbMapping -LocalPath G: -RemotePath \\LON-DC1\MyRepo\My-AZ-040-LabFiles
 New-SmbMapping -LocalPath H: -RemotePath \\LON-DC1\MyRepo

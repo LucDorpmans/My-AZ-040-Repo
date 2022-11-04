a87f5cbdaf78e6d42f2cbd57693dd2d629bda55a
LucDorpmans <GitHub@sure.nl>
2022-11-04 22:52:04 +0100

    Upd

--- My-AZ-040-AllFiles/Mod11/2-2 - Scheduled Jobs.ps1
+++ My-AZ-040-AllFiles/Mod11/2-2 - Scheduled Jobs.ps1
@@ -1,28 +1,29 @@
-﻿
-# Demonstration: Using scheduled jobs
+﻿# Demonstration: Using scheduled jobs
 
 Import-Module PSScheduledJob
 Get-Job | Remove-Job
 
 $trigger = New-JobTrigger -Once -At (Get-Date).AddMinutes(2)
 
-Register-ScheduledJob -Trigger $trigger -Name DemoJob -ScriptBlock { Get-EventLog -LogName Application }
+$SchedJobName = "DemoJob"
+Register-ScheduledJob -Trigger $trigger -Name $SchedJobName -ScriptBlock { Get-EventLog -LogName Application -Newest 10 }
 
 Get-ScheduledJob | Select-Object -Expand JobTriggers 
-# notice the Time
+# Notice the time
 
 Get-ScheduledJob 
 # wait until after the time that was displayed in the previous step.
 
 Get-Job
 
-Receive-Job -Name DemoJob
+Receive-Job -Name $SchedJobName
 
-Get-Job -Name DemoJob | Remove-Job
+Get-Job -Name $SchedJobName | Remove-Job
 
+Get-ChildItem "$env:HOMEPATH\AppData\Local\Microsoft\Windows\PowerShell\ScheduledJobs" -Recurse
+# Get-ChildItem C:\Users\Administrator.ADATUM\AppData\Local\Microsoft\Windows\PowerShell\ScheduledJobs -Recurse
+PSEdit "C:\Users\Administrator.ADATUM\AppData\Local\Microsoft\Windows\PowerShell\ScheduledJobs\DemoJob\ScheduledJobDefinition.xml"
 
-
-
-
-
+#Cleanup (for next time)
+Get-ScheduledJob -Name $SchedJobName | Unregister-ScheduledJob  
 

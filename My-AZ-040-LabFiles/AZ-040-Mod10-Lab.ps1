57a5907d2a12493aed72636cf991275bf1bf7bb1
LucDorpmans <GitHub@sure.nl>
2022-11-04 15:02:22 +0100

    Upd

--- My-AZ-040-LabFiles/AZ-040-Mod10-Lab.ps1
+++ My-AZ-040-LabFiles/AZ-040-Mod10-Lab.ps1
@@ -11,10 +11,7 @@
 # These can be found in the All Files\ E: on the AZ-040T00A-LON-CL1 VM
 
 # Task 1: Connect to Azure AD
-On AZ-040T00A-LON-CL1, select Start, and then Enter powershell.
-
-In the results list, right-click Windows PowerShell or activate its context menu, and then select Run as administrator.
-
+# On AZ-040T00A-LON-CL1:
 # To install the AzureAD module, in the Administrator: Windows PowerShell console, enter the following command, and then press the Enter key (when prompted for confirmation, Enter Y and press the Enter key again twice in a row):
 Install-Module AzureAD
 
@@ -44,7 +41,7 @@ New-AzureADUser -DisplayName "Noreen Riggs" -UserPrincipalName Noreen@$verifiedD
 $user = Get-AzureADUser -ObjectID Noreen@$verifiedDomain
 
 # To store a reference to the Global Administrator role in a variable:
-$role = Get-AzureADDirectoryRole | Where {$_.displayName -eq 'Global Administrator'}
+$role = Get-AzureADDirectoryRole | Where-Object {$_.displayName -eq 'Global Administrator'}
 
 # To assign the Global Administrator role to Noreen's user account:
 Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId $user.ObjectID
@@ -63,10 +60,10 @@ New-AzureADUser -DisplayName "Allan Yoo" -UserPrincipalName Allan@$verifiedDomai
 Set-AzureADUser -ObjectId Allan@$verifiedDomain -UsageLocation US
 
 # To review the available licenses in the tenant:
-Get-AzureADSubscribedSku | FL
+Get-AzureADSubscribedSku | Format-List
 
 # To store the SKU ID for the intended license in a variable:
-$SkuId = (Get-AzureADSubscribedSku | Where SkuPartNumber -eq "ENTERPRISEPREMIUM").SkuID
+$SkuId = (Get-AzureADSubscribedSku | Where-Object SkuPartNumber -eq "ENTERPRISEPREMIUM").SkuID
 
 # To create an AssignedLicense object:
 $License = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
@@ -133,8 +130,10 @@ Set-CalendarProcessing BoardRoom -AutomateProcessing AutoAccept
 # Task 3: Verify room resource booking
 # On AZ-040T00A-LON-CL1, to open the outlook site in Microsoft Edge:
 Start-Process https://outlook.office.com
-# Sign in as Allan Yoo by using the UserPrincipalName as the user name and providing the password you recorded in the previous exercise of this lab. 
-# When prompted, change your password as instructed. 
+# Sign in as Allan Yoo by using the UserPrincipalName as the user name and providing the password 
+# P@ssw0rd1234 
+# When prompted, change your password as instructed
+# P@ss221104
 # Be sure to record the password so that you can use it during subsequent exercises.
 # If prompted to stay signed in, select No.
 # From the menu bar, select Calendar, and then select New event.
@@ -172,7 +171,7 @@ New-SPOSite -Url https://$verifiedDomainShort.sharepoint.com/sites/Sales -Owner 
 # The -NoWait parameter performs this task asynchronously, so you don't need to wait for its completion. 
 # If you intend to wait, you can verify the status of the SharePoint site by entering the following command, 
 # and then pressing the Enter key:
-Get-SPOSite | FL Url,Status
+Get-SPOSite | Format-List Url,Status
 
 # To disconnect from SharePoint Online:
 Disconnect-SPOService
@@ -183,12 +182,13 @@ Disconnect-SPOService
 ###
 # Task 1: Connect to Microsoft Teams
 # To install the Microsoft Teams PowerShell Module, in the Administrator: Windows PowerShell console, enter the following command, and then press the Enter key (when prompted for confirmation, Enter A and press the Enter key again):
-Install-Module MicrosoftTeams
+Install-Module MicrosoftTeams -Force
 
 # To connect to Microsoft Teams:
 Connect-MicrosoftTeams
 
 # In the Sign in to your account window, select the Use another account option, enter the name of the user account with the Global Administrator role in the Azure Active Directory (Azure AD) tenant you were using in this lab, and then select Next.
+
 # To verify that there are no existing teams:
 Get-Team
 
@@ -202,7 +202,7 @@ New-Team -DisplayName "Sales Team" -MailNickName "SalesTeam"
 $team = Get-Team -DisplayName "Sales Team"
 
 # To review the information about your team:
-$team | FL
+$team | Format-List
 # Review the information about the Sales Team. Notice that GroupId is a unique identifier.
 
 # To add a user to the team:
